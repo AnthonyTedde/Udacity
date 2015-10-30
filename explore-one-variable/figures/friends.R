963eac6d0d12e909c77ad7926c1b289fbc7a9135
elpapito <antho.tedde@gmail.com>
2015-10-30 15:01:08 +0100

    Update plot for exercice

--- explore-one-variable/figures/friends.R
+++ explore-one-variable/figures/friends.R
@@ -8,13 +8,31 @@ tikz('friends.tex',
      width = 5,
      height = 5)
 
-qplot(data = subset(x = pf, subset = !is.na(gender)),
-      x = friend_count,
-      binwidth = 25) +
+###########################################################
+## qplot(data = subset(x = pf, subset = !is.na(gender)), ##
+##       x = friend_count,                               ##
+##       binwidth = 25) +                                ##
+##     scale_x_continuous(limits = c(0, 1000),           ##
+##                        breaks = seq(0,1000,100)) +    ##
+##     facet_wrap(~gender, ncol = 1) +                   ##
+##     stat_summary(fun.x = "median", geom = "point") +  ##
+##     theme_bw()                                        ##
+###########################################################
+
+med <- median(x = pf$friend_count,
+              na.rm = T)
+av <- mean(x = pf$friend_count)
+ggplot(data = pf,
+       aes(x = friend_count)) +
+    geom_histogram() +
+    stat_bin(binwidth = 25) +
     scale_x_continuous(limits = c(0, 1000),
-                       breaks = seq(0,1000,100)) +
-    facet_wrap(~gender, ncol = 1) +
-    theme_bw()
+                       breaks = seq(0, 1000, 100)) +
+    geom_vline(xintercept = med,
+               colour = "red") +
+    geom_vline(xintercept = av,
+               colour = "blue") +
+    theme_hc()
 
 dev.off()
 
---
+++ explore-one-variable/init.R
@@ -0,0 +1,1 @@
+setwd('~/workspace/udacity/explore-one-variable/')
